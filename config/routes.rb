88a8ab8a1cdc7f6cf1c48d40be3b217319ec5f51
yosifgenchev <r.genchew@gmail.com>
2016-07-31 21:26:46 +0300

    Add help and contacts pages to routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  
+  #root 'application#hello'
+
+  root 'static_pages#help'
+
+  get  '/help', to: 'static_pages#help'
+  get  '/contacts', to: 'static_pages#contacts'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
