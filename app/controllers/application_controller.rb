d0d8b796a58d02ebd4be668b0dbde441566248dc
yosifgenchev <r.genchew@gmail.com>
2016-08-02 17:11:25 +0300

    Add Login and Signup tests, Test Helper, omniauth.rb for the Facebook login and Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  def hello
-  	render html: "Hello, this is PlayIt!"
+  include SessionsHelper
+
+  private
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+  helper_method :current_user
 end
