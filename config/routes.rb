819b16f6369f4beb7e7023679d32d9e6fe7fe901
yosifgenchev <r.genchew@gmail.com>
2016-08-02 16:03:33 +0300

    Add some routes (help, contacts, home, signup, login, logout) + some routes for Facebook Login

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,30 @@
 Rails.application.routes.draw do
-  
-  #root 'application#hello'
 
-  root 'static_pages#help'
+  get 'sessions/new'
 
-  get  '/help', to: 'static_pages#help'
-  get  '/contacts', to: 'static_pages#contacts'
+  get 'users/new'
+
+  root 'homepage#home'
+
+  get    '/help', 		to: 'static_pages#help'
+  get    '/contacts', 	to: 'static_pages#contacts'
+  get    '/home', 		to: 'homepage#home'
+  get    '/signup',		to: 'users#new'
+  post 	 '/signup',  	to: 'users#create'
+  get    '/login',   	to: 'sessions#new'
+  post   '/login',   	to: 'sessions#create'
+  delete '/logout',  	to: 'sessions#destroy'
+
+  resources :users
+
+  get 'auth/:provider/callback', to: 'sessions#createFacebook'
+  get 'auth/failure', to: redirect('/')
+  get 'signout', to: 'sessions#destroyFacebook', as: 'signout'
+
+  resources :sessions, only: [:create, :destroy]
+  resource :home, only: [:show]
+
+  root to: "homepage#home"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
