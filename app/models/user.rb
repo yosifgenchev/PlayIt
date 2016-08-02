b0da65564ac44c7b8e7b0ab9d8a520c8b3f8fe16
yosifgenchev <r.genchew@gmail.com>
2016-08-02 21:31:13 +0300

    Add has_many relation to events

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :events, dependent: :destroy
   attr_accessor :remember_token
 	before_save { self.email = email.downcase }
 	validates :name,  presence: true, 
