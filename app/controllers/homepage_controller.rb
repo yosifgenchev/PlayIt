e1e2d96066a7ad7eb975107700fc8743b0974e52
yosifgenchev <r.genchew@gmail.com>
2016-08-08 18:20:44 +0300

    Filter and render all basketball events (TBR = To be removed)

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -4,5 +4,11 @@ class HomepageController < ApplicationController
   	 User.all.each do |user|
   	 	@events = @events + user.events
   	 end
+
+  	 @basketballEvents = []
+  	 @events.each do |event|
+  	 	@basketballEvents << event if event.sport.name == 'Basketball'
+  	 end
+
   end
 end
