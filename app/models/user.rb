8d5fd0d982e1d1f2a4e94c9bf1435920bfa20084
yosifgenchev <r.genchew@gmail.com>
2016-09-10 13:11:40 +0300

    Some small changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,21 @@ class User < ApplicationRecord
   has_many :attended_events, through: :userevents
 
   attr_accessor :remember_token
-	# before_save { self.email = email.downcase }
+	 before_save { self.email = email.downcase }
   mount_uploader :picture, PictureUploader
-	# validates :name,  presence: true, 
-	# 				  length: { maximum: 50 }
+	 validates :name,  presence: true, 
+	 				  length: { maximum: 50 }
 
-	# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
- #  	validates :email, presence: true, 
- #  					  length: { maximum: 255 },
- #  				      format: { with: VALID_EMAIL_REGEX },
- #  				      uniqueness: { case_sensitive: false }
+	 VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+   	validates :email, presence: true, 
+   					  length: { maximum: 255 },
+   				      format: { with: VALID_EMAIL_REGEX },
+   				      uniqueness: { case_sensitive: false }
 
- # 	validates :password, presence: true, 
- # 						 length: { minimum: 6 }
+  	validates :password, presence: true, 
+  						 length: { minimum: 6 }
 
- 	# has_secure_password
+ 	 has_secure_password
 
   validate  :picture_size
 
@@ -59,6 +59,8 @@ class User < ApplicationRecord
       user.provider = auth.provider
       user.uid = auth.uid
       user.name = auth.info.name
+      user.email = auth.info.email
+      user.picture = auth.info.image
       user.oauth_token = auth.credentials.token
       user.oauth_expires_at = Time.at(auth.credentials.expires_at)
       user.save!
